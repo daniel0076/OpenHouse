e69b99b9674c7a4b1d461d774d9d10c1844decaa
Daniel Tsai <idaniel.twc@gmail.com>
2016-05-25 23:56:12 +0800

    說明會model及admin page完成

--- rdss/admin.py
+++ rdss/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from rdss import models
 
 # Register your models here.
+admin.site.register(models.Activity)
+admin.site.register(models.Seminar_Info)
+admin.site.register(models.Seminar_Slot)
+admin.site.register(models.Seminar_Order)
--- rdss/models.py
+++ rdss/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Activity(models.Model):
 	id = models.AutoField(primary_key=True)
 	cid = models.CharField(u'公司統一編號',unique=True,max_length=8)
-	name = models.CharField(u'公司名稱',max_length=64)
 	seminar_noon = models.BooleanField(u'說明會中午')
 	seminar_night = models.BooleanField(u'說明會晚上')
 	enterprise_day= models.BooleanField(u'專屬企業日')
@@ -17,8 +16,59 @@ class Activity(models.Model):
 	class Meta:
 		managed = True
 
-		verbose_name = u"研替活動狀況"
-		verbose_name_plural =u"研替活動狀況"
+		verbose_name = u"研替活動報名"
+		verbose_name_plural =u"研替活動報名"
 
 	def __str__(self):
-		return self.name
+		return self.shortname
+
+class Seminar_Slot(models.Model):
+
+	id = models.AutoField(primary_key=True)
+	time = models.DateTimeField(u'場次時間')
+	cid=models.OneToOneField('Activity',to_field='cid',on_delete=models.CASCADE)
+	updated = models.DateTimeField(u'更新時間',auto_now=True)
+
+	class Meta:
+		managed = True
+		verbose_name = u"說明會場次"
+		verbose_name_plural =u"說明會場次"
+	def __str__(self):
+		return self.cid
+
+class Seminar_Order(models.Model):
+	id = models.AutoField(primary_key=True)
+	time = models.DateTimeField(u'選位開始時間')
+	cid=models.OneToOneField('Activity',to_field='cid',on_delete=models.CASCADE)
+	updated = models.DateTimeField(u'更新時間',auto_now=True)
+
+	class Meta:
+		managed = True
+		verbose_name = u"說明會選位順序"
+		verbose_name_plural =u"說明會選位順序"
+
+class Seminar_Info(models.Model):
+	id = models.AutoField(primary_key=True)
+	cid=models.OneToOneField('Activity',to_field='cid',on_delete=models.CASCADE)
+	topic = models.CharField(u'說明會主題',max_length=30)
+	speaker = models.CharField(u'主講人',max_length=30)
+	speaker_title = models.CharField(u'主講人稱謂',max_length=30)
+	speaker_email = models.EmailField(u'主講人Email',max_length=254)
+	attendees = models.SmallIntegerField(u'廠商到場人數')
+	raffle_prize = models.TextField(u'抽獎獎品')
+	raffle_prize_amount = models.SmallIntegerField(u'抽獎獎品數量')
+	qa_prize = models.TextField(u'QA獎獎品')
+	qa_prize_amount = models.SmallIntegerField(u'QA獎獎品數量')
+	attend_prize = models.TextField(u'參加獎獎品')
+	attend_prize_amount = models.SmallIntegerField(u'參加獎獎品數量')
+	snack_box = models.SmallIntegerField(u'加碼餐盒數量')
+	contact = models.CharField(u'聯絡人',max_length=30)
+	contact_mobile = models.CharField(u'聯絡人手機',max_length=16)
+	contact_email = models.EmailField(u'聯絡人Email',max_length=254)
+	ps = models.EmailField(u'其它需求',max_length=254)
+	updated = models.DateTimeField(u'更新時間',auto_now=True)
+
+	class Meta:
+		managed = True
+		verbose_name = u"說明會資訊"
+		verbose_name_plural =u"說明會資訊"
