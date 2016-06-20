8fddb816016785106febb292e2082985c1c0b570
Daniel Tsai <idaniel.twc@gmail.com>
2016-06-21 00:56:47 +0800

    merge to rdss app

--- oh_generic/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Configs(models.Model):
-	signup_start = models.DateTimeField(u'廠商註冊開始時間')
-	signup_end = models.DateTimeField(u'廠商註冊結束時間')
-	rdss_signup_start = models.DateTimeField(u'研替報名開始時間')
-	rdss_signup_end = models.DateTimeField(u'研替報名結束時間')
