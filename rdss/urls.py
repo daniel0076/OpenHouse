75d957f35db6678d6fced2c7cd5036224d360e00
Daniel Tsai <idaniel.twc@gmail.com>
2016-06-20 18:51:46 +0800

    modified model structure, add views for other page

--- rdss/urls.py
+++ rdss/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # url(r'^$', 'oh2016_dj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^',views.ControlPanel,name="rdss_index"),
+    url(r'^$',views.ControlPanel,name="rdss_index"),
+    url(r'^signup/$',views.SignupActivity,name="rdss_act_signup"),
+    url(r'^seminar/$',views.SeminarInfo,name="rdss_seminar"),
+    url(r'^jobfair/$',views.JobfairInfo,name="rdss_jobfair"),
 ]
--- rdss/views.py
+++ rdss/views.py
@@ -3,6 +3,7 @@ from django.http import  HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+import rdss.forms
 
 # Create your views here.
 
@@ -10,3 +11,18 @@ from django.contrib.auth.decorators import login_required
 def ControlPanel(request):
 	nav_rdss="active"
 	return render(request,'rdss.html',locals())
+
+@login_required(login_url='/company/login/')
+def SignupActivity(request):
+	form = rdss.forms.ActivityCreationForm
+	return render(request,'activity_form.html',locals())
+
+@login_required(login_url='/company/login/')
+def SeminarInfo(request):
+	form = rdss.forms.SeminarInfoCreationForm
+	return render(request,'seminar_info_form.html',locals())
+
+@login_required(login_url='/company/login/')
+def JobfairInfo(request):
+	form = rdss.forms.JobfairInfoCreationForm
+	return render(request,'jobfair_info_form.html',locals())
