31090682d3ac508b139fb1d29755b878ad99564f
Daniel Tsai <idaniel.twc@gmail.com>
2016-07-07 12:00:05 +0800

    working on staff creation

---
+++ staff/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+import staff.models
+from django.utils import timezone
+
+class StaffCreationForm(forms.ModelForm):
+	error_messages={
+			'password_mismatch': ('兩次輸入的密碼不一樣'),
+			'cid_error': ('統一編號必需都是數字'),
+			}
+	#customed fields
+	password1 = forms.CharField(label=(u'密碼'), widget=forms.PasswordInput)
+	password2 = forms.CharField(label=(u'密碼確認'),
+			widget=forms.PasswordInput, help_text=('請再次輸入密碼'))
+
+	required_css_class = 'required'
+
+	class Meta:
+		model=staff.models.Staff
+		fields='__all__'
+		exclude=['id','password','last_login','is_active']
+
+	def clean_password2(self):
+		password1=self.cleaned_data.get('password1')
+		password2=self.cleaned_data.get('password2')
+		if password1 and password2 and password1!=password2:
+			raise forms.ValidationError(
+					self.error_messages['password_mismatch'],
+					code='password_mismatch'
+					)
+			return password2
--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import staff.forms
 
 # Create your views here.
+
+def StaffCreation(request):
+	form = staff.forms.StaffCreationForm
+	return render(request,'staff_form.html',{'form':form})
