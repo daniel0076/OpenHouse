76bc1a25231873227d98cc96dd7acab111a6da6b
Daniel Tsai <idaniel.twc@gmail.com>
2016-07-23 00:35:15 +0800

    add news permission

--- general/models.py
+++ general/models.py
@@ -7,15 +7,21 @@ from ckeditor_uploader.fields import RichTextUploadingField
 class News(models.Model):
 	CATEGORYS = (
 			(u'最新消息', u'最新消息'),
-			(u'廠商專區', u'廠商專區')
+			(u'徵才專區', u'徵才專區')
+			)
+	PERM = (
+			(u'index_only', u'只顯示於首頁'),
+			(u'company_only', u'只顯示於廠商'),
+			(u'both', u'顯示於首頁及皮廠商'),
 			)
 	id = models.AutoField(primary_key=True)
 	title = models.CharField(u'標題',max_length=100)
 	category = models.CharField(u'公告類別',max_length=5,choices=CATEGORYS)
+	perm = models.CharField(u'誰能看到這則公告',max_length=15,choices=PERM)
 	content = RichTextUploadingField(u'內容')
 	created_time = models.DateTimeField(u'發佈時間',auto_now_add=True)
 	updated_time = models.DateTimeField(u'更新時間',auto_now=True)
 
 	class Meta:
-		verbose_name = "公告"
-		verbose_name_plural = "公告"
+		verbose_name = "公告系統"
+		verbose_name_plural = "公告系統"
--- general/views.py
+++ general/views.py
@@ -5,17 +5,31 @@ from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.utils import timezone
+from django.db.models import Q
 import datetime,json
 from . import models
 
 def Index(request):
-	general_news_list = models.News.objects.filter(category="最新消息")
-	company_news_list = models.News.objects.filter(category="廠商專區")
+	general_news_list = models.News.objects.filter(category="最新消息").exclude(perm="company_only")
+	recruit_news_list = models.News.objects.filter(category="徵才專區").exclude(perm="company_only")
 	return render(request,'general/index.html',locals())
 
+#TODO permission
 def ReadNews(request,news_id):
 	news = models.News.objects.filter(id = news_id).first()
 	if not news:
 		raise Http404("Not found")
 	return render(request,'general/read_news.html',locals())
+
+@login_required
+def GetCompanyNewsList(request):
+	news_list = models.News.objects.filter(Q(perm="both") | Q(perm="company_only"))
+
+	ret_news_list = []
+	for news in news_list:
+		news_dict = {"title":news.title,"updated_time":news.updated_time,"url":"/news/"+news.id}
+		ret_news_list.append(news_dict)
+
+	return JsonResponse({'success':True,'data':ret_news_list})
+
 
