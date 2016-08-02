52a65692549e7b189335958974182b858e60e203
Daniel Tsai <idaniel.twc@gmail.com>
2016-08-03 02:04:46 +0800

    fix security issue in password reset

--- company/forms.py
+++ company/forms.py
@@ -9,117 +9,121 @@ from django.utils.encoding import force_bytes
 from django.utils.translation import ugettext as _
 class CompanyCreationForm(forms.ModelForm):
 
-	error_messages={
-			'password_mismatch': ('兩次輸入的密碼不一樣'),
-			'cid_error': ('統一編號必需都是數字'),
-			}
-	#customed fields
-	cid= forms.CharField(label=(u'公司統一編號'),help_text='請輸入8位數統一編號')
-	password1 = forms.CharField(label=(u'密碼'), widget=forms.PasswordInput)
-	password2 = forms.CharField(label=(u'密碼確認'),
-			widget=forms.PasswordInput, help_text=('請再次輸入密碼'))
+    error_messages={
+            'password_mismatch': ('兩次輸入的密碼不一樣'),
+            'cid_error': ('統一編號必需都是數字'),
+            }
+    #customed fields
+    cid= forms.CharField(label=(u'公司統一編號'),help_text='請輸入8位數統一編號')
+    password1 = forms.CharField(label=(u'密碼'), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=(u'密碼確認'),
+            widget=forms.PasswordInput, help_text=('請再次輸入密碼'))
 
-	required_css_class = 'required'
+    required_css_class = 'required'
 
-	def __init__(self, *args, **kwargs):
-		super(CompanyCreationForm, self).__init__(*args, **kwargs)
-		self.fields['category'].widget.attrs.update({
-				'class': 'ui dropdown',
-				})
+    def __init__(self, *args, **kwargs):
+        super(CompanyCreationForm, self).__init__(*args, **kwargs)
+        self.fields['category'].widget.attrs.update({
+                'class': 'ui dropdown',
+                })
 
-	class Meta:
-		model=Company
-		fields='__all__'
-		exclude=['id','password','last_login','is_active']
-		widgets = {
-				'brief': forms.Textarea(attrs={'cols': 80, 'rows': 8}),
-				'introduction': forms.Textarea(attrs={'cols': 80, 'rows': 8}),
-				}
+    class Meta:
+        model=Company
+        fields='__all__'
+        exclude=['id','password','last_login','is_active']
+        widgets = {
+                'brief': forms.Textarea(attrs={'cols': 80, 'rows': 8}),
+                'introduction': forms.Textarea(attrs={'cols': 80, 'rows': 8}),
+                }
 
-	class Media:
-		js = ('js/company/company_create_form.js',)
-		css = {
-				'all':('css/company/company_create_form.css',),
-				}
+    class Media:
+        js = ('js/company/company_create_form.js',)
+        css = {
+                'all':('css/company/company_create_form.css',),
+                }
 
-	def clean_cid(self):
-		cid=self.cleaned_data.get('cid')
-		if not cid.isdigit():
-			raise forms.ValidationError(
-					self.error_messages['cid_error'],
-					code='cid_error'
-					)
-		return cid
+    def clean_cid(self):
+        cid=self.cleaned_data.get('cid')
+        if not cid.isdigit():
+            raise forms.ValidationError(
+                    self.error_messages['cid_error'],
+                    code='cid_error'
+                    )
+        return cid
 
-	def clean_password2(self):
-		password1=self.cleaned_data.get('password1')
-		password2=self.cleaned_data.get('password2')
-		if password1 and password2 and password1!=password2:
-			raise forms.ValidationError(
-					self.error_messages['password_mismatch'],
-					code='password_mismatch'
-					)
-		return password2
+    def clean_password2(self):
+        password1=self.cleaned_data.get('password1')
+        password2=self.cleaned_data.get('password2')
+        if password1 and password2 and password1!=password2:
+            raise forms.ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code='password_mismatch'
+                    )
+        return password2
 
-	def save(self,commit=True):
-		user = super(CompanyCreationForm, self).save(commit=False)
-		user.last_login=timezone.now()
-		user.set_password(self.cleaned_data['password2'])
-		if commit:
-			user.save()
-		return user
+    def save(self,commit=True):
+        user = super(CompanyCreationForm, self).save(commit=False)
+        user.last_login=timezone.now()
+        user.set_password(self.cleaned_data['password2'])
+        if commit:
+            user.save()
+        return user
 
 class CompanyEditForm(forms.ModelForm):
-	def __init__(self, *args, **kwargs):
-		super(CompanyEditForm, self).__init__(*args, **kwargs)
-		self.fields['category'].widget.attrs.update({
-				'class': 'ui dropdown',
-				})
+    def __init__(self, *args, **kwargs):
+        super(CompanyEditForm, self).__init__(*args, **kwargs)
+        self.fields['category'].widget.attrs.update({
+                'class': 'ui dropdown',
+                })
 
-		required_css_class = 'required'
+        required_css_class = 'required'
 
-	class Meta:
-		model=Company
-		fields='__all__'
-		exclude=['password','last_login','date_join','is_active']
-		widgets = {
-				'brief': forms.Textarea(),
-				'introduction': forms.Textarea(),
-				}
+    class Meta:
+        model=Company
+        fields='__all__'
+        exclude=['password','last_login','date_join','is_active']
+        widgets = {
+                'brief': forms.Textarea(),
+                'introduction': forms.Textarea(),
+                }
 
-	#css/js for the form, need to put {{form.media}} in template
-	class Media:
-		js = ('js/company/company_edit_form.js',)
-		css = {
-				'all':('css/company/company_edit_form.css',),
-				}
+    #css/js for the form, need to put {{form.media}} in template
+    class Media:
+        js = ('js/company/company_edit_form.js',)
+        css = {
+                'all':('css/company/company_edit_form.css',),
+                }
 
 #ref here https://docs.djangoproject.com/en/1.8/ref/forms/validation/  search clean_
-	def save(self,commit=True):
-		user = super(CompanyEditForm, self).save(commit=False)
-		if commit:
-			user.save()
-		return user
+    def save(self,commit=True):
+        user = super(CompanyEditForm, self).save(commit=False)
+        if commit:
+            user.save()
+        return user
 class CompanyPasswordResetForm(PasswordResetForm):
     error_messages = {
         'does_not_exist':_("這個帳號不存在"),
     }
     user = forms.CharField(max_length=8)
-    email = None
+    to_email = None
     def save(self,request=None):
         current_site = get_current_site(request)
         company = Company.objects.get(cid=self.cleaned_data['user'])
-        email = company.hr_email
+        to_email = company.hr_email
         context = {
-            'email': email,
+            'email': to_email,
             'domain': current_site.domain,
             'site_name': current_site.name,
             'uid': urlsafe_base64_encode(force_bytes(company.pk)),
             'user':company,
             'token': default_token_generator.make_token(company),
-            'protocol': 'http'
+            'protocol': 'https',
+            'company_name':company.shortname
         }
-        self.send_mail('password_reset_subject.txt','password_reset_email.html',context,'skye53653@gmail.com',email)
+        subject_template = "password_reset_subject.txt"
+        email_body_template = 'password_reset_email.html'
+        from_email = "交通大學 OpenHouse <password_reset@openhouse.nctu.edu.tw>"
+        self.send_mail(subject_template,email_body_template,context,from_email,to_email)
 
     def clean_user(self):
         user = self.cleaned_data.get('user')
--- company/views.py
+++ company/views.py
@@ -17,88 +17,94 @@ import company.models
 @login_required(login_url='/company/login/')
 def CompanyIndex(request):
 
-	#semantic ui control
-	nav_company_index = "active"
+    #semantic ui control
+    nav_company_index = "active"
 
-	# rdss files
-	rdss_file_list = rdss.models.Files.objects.all()
-	return render(request,'company_index.html',locals())
+    # rdss files
+    rdss_file_list = rdss.models.Files.objects.all()
+    return render(request,'company_index.html',locals())
 
 @login_required(login_url='/company/login/')
 def CompanyInfo(request):
-	company_info = company.models.Company.objects.get(cid=request.user.cid)
-	return render(request,'company_info.html',locals())
+    company_info = company.models.Company.objects.get(cid=request.user.cid)
+    return render(request,'company_info.html',locals())
 
 def CompanyCreation(request):
-	submit_btn_name = "創建帳號"
-	if request.POST:
-		form = CompanyCreationForm(request.POST,request.FILES)
-		if form.is_valid():
-			form.save()
-			user = authenticate(username=form.clean_cid(), password=form.clean_password2())
-			login(request,user)
-	#       messages.success(request, _("User '{0}' created.").format(user))
-			return redirect(CompanyIndex)
-		else:
-			error_display = True
-			error_msg = form.errors
-			#messages.error(request, ("The user could not be created due to errors.") )
-			return render(request,'company_create_form.html',locals())
-	form = CompanyCreationForm();
-	return render(request,'company_create_form.html',locals())
+    submit_btn_name = "創建帳號"
+    if request.POST:
+        form = CompanyCreationForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            user = authenticate(username=form.clean_cid(), password=form.clean_password2())
+            login(request,user)
+    #       messages.success(request, _("User '{0}' created.").format(user))
+            return redirect(CompanyIndex)
+        else:
+            error_display = True
+            error_msg = form.errors
+            #messages.error(request, ("The user could not be created due to errors.") )
+            return render(request,'company_create_form.html',locals())
+    form = CompanyCreationForm();
+    return render(request,'company_create_form.html',locals())
 
 @login_required(login_url='/company/login/')
 def CompanyEdit(request):
-	submit_btn_name = "確認修改"
-	if request.user and request.user.is_authenticated():
-		user=request.user
-	else: user=None
-	if request.POST:
-		form = CompanyEditForm(request.POST,request.FILES,instance=user)
-		if form.is_valid():
-			user = form.save()
-	#        messages.success(request, _("User '{0}' created.").format(user))
-			return redirect(CompanyInfo)
-		else:
-			print(form.errors)
-			#messages.error(request, ("The user could not be created due to errors.") )
-			return render(request,'company_edit_form.html',locals())
-	form = CompanyEditForm(instance=user);
-	company_info = company.models.Company.objects.get(cid=request.user.cid)
-	return render(request,'company_edit_form.html',locals())
+    submit_btn_name = "確認修改"
+    if request.user and request.user.is_authenticated():
+        user=request.user
+    else: user=None
+    if request.POST:
+        form = CompanyEditForm(request.POST,request.FILES,instance=user)
+        if form.is_valid():
+            user = form.save()
+    #        messages.success(request, _("User '{0}' created.").format(user))
+            return redirect(CompanyInfo)
+        else:
+            print(form.errors)
+            #messages.error(request, ("The user could not be created due to errors.") )
+            return render(request,'company_edit_form.html',locals())
+    form = CompanyEditForm(instance=user);
+    company_info = company.models.Company.objects.get(cid=request.user.cid)
+    return render(request,'company_edit_form.html',locals())
 
 def CompanyLogin(request):
 
-	if request.POST:
-		username=request.POST.get('username')
-		password=request.POST.get('password')
+    if request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
 
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			if user.is_active:
-				login(request,user)
-			if user.is_staff:
-				return redirect('/admin/')
-			else:
-				return redirect('/company/')
-		else:
-			error_display = True
-			error_msg = "帳號或密碼錯誤"
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request,user)
+            if user.is_staff:
+                return redirect('/admin/')
+            else:
+                return redirect('/company/')
+        else:
+            error_display = True
+            error_msg = "帳號或密碼錯誤"
 
-	return render(request,'login.html',locals())
+    return render(request,'login.html',locals())
 
 def CompanyLogout(request):
-	logout(request)
-	return redirect('/company/login/')
+    logout(request)
+    return redirect('/company/login/')
 
 
 def forget_password(request):
     send = None
     if request.method == 'POST':
-        form = CompanyPasswordResetForm(request.POST)
+        data = request.POST.copy()
+        data['email'] = "dummy@e.mail"
+        form = CompanyPasswordResetForm(data)
         if form.is_valid():
             form.save(request=request)
-            return redirect('/company/login/')
+            company_obj = company.models.Company.objects.get(cid=form.cleaned_data.get('user'))
+            print(company_obj)
+            return render(request,'notify_password_reset.html',locals())
+        else:
+            print(form.errors)
     else:
         form = CompanyPasswordResetForm()
     return render(request,'forget_password.html',{'form':form,'send':send})
@@ -128,11 +134,11 @@ def password_reset_confirm(request,uidb64,token):
     return redirect('/')
 
 def ResetPassword(request):
-	user = Company.objects.get(cid=request.user.cid)
-	if request.method == 'POST':
-		form = SetPasswordForm(user,request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect(CompanyInfo)
-	form = SetPasswordForm(user)
-	return render(request,'password_reset_confirm.html',{'form': form})
+    user = Company.objects.get(cid=request.user.cid)
+    if request.method == 'POST':
+        form = SetPasswordForm(user,request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(CompanyInfo)
+    form = SetPasswordForm(user)
+    return render(request,'password_reset_confirm.html',{'form': form})
