02267c0e517ebada12cbab06c1e336bda3274a0f
Daniel Tsai <idaniel.twc@gmail.com>
2016-08-14 00:07:07 +0800

    use new image slider, update gitignore

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,22 @@
+
 $( document ).ready(function() {
-	$('.dropdown').dropdown();
-	$('#rdss_navtop_dropdown')
-		.dropdown({
-			on: 'hover'
-		})
-	;
+  var slider = new IdealImageSlider.Slider({
+    selector: '#slider',
+    height: 400, // Required but can be set by CSS
+    interval: 3000
+  });
+
+  slider.addCaptions();
+
+  $(window).focus(function() {
+    slider.start();
+  });
+
+
+  $('.dropdown').dropdown();
+  $('#rdss_navtop_dropdown')
+    .dropdown({
+      on: 'hover'
+    })
+  ;
 });
