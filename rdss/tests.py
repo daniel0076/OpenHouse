5c863b9d83b168b5d8ebcf1ffd07277d7a873634
Daniel Tsai <idaniel.twc@gmail.com>
2016-09-02 13:24:46 +0800

    Update app general tests and rdss tests

--- rdss/tests.py
+++ rdss/tests.py
@@ -7,16 +7,16 @@ import company.models
 # Create your tests here.
 
 class LoginReqTest(TestCase):
-	def setUp(self):
-		# Every test needs access to the request factory.
-		self.factory = RequestFactory()
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
 
-	def test_anony(self):
-		# Create an instance of a GET request.
-		request = self.factory.get('/rdss/status')
-		# an AnonymousUser instance.
-		request.user = AnonymousUser()
-		# Test my_view() as if it were deployed at /customer/details
-		response = rdss.views.Status(request)
-		# Use this syntax for class-based views.
-		self.assertEqual(response.status_code, 302)
+    def test_anony(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/rdss/status')
+        # an AnonymousUser instance.
+        request.user = AnonymousUser()
+        # Test my_view() as if it were deployed at /customer/details
+        response = rdss.views.Status(request)
+        # Use this syntax for class-based views.
+        self.assertEqual(response.status_code, 302)
