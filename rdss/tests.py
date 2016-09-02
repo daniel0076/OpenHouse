30bcf99f9a0543d7ff38ca19fa66b2700cf2692c
Daniel Tsai <idaniel.twc@gmail.com>
2016-09-02 20:03:11 +0800

    Add rdss tests

--- rdss/tests.py
+++ rdss/tests.py
@@ -1,10 +1,23 @@
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
+from rdss.public_urls import urlpatterns as pub_urls
+from rdss.internal_urls import urlpatterns as inter_urls
 
 import rdss.views
 import company.models
 
-# Create your tests here.
+class UrlsTest(TestCase):
+
+    def test_public(self):
+        for url in pub_urls:
+            response = self.client.get(reverse(url.name))
+            self.assertEqual(response.status_code, 200)
+
+    def test_internal(self):
+        for url in inter_urls:
+            response = self.client.get(reverse(url.name))
+            self.assertEqual(response.status_code, 302)
 
 class LoginReqTest(TestCase):
     def setUp(self):
