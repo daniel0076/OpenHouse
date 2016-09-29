9c43c4454448c33048ae3995ca5221c84bdee4c5
Daniel Tsai <idaniel.twc@gmail.com>
2016-09-29 14:34:48 +0800

    Fix auto submit form, use timeout, 250ms

--- static/js/collect_points.js
+++ static/js/collect_points.js
@@ -1,18 +1,28 @@
+
+
+
 $( document ).ready(function() {
     $('.ui.dropdown').dropdown();
     $('input[name="idcard_no"]').focus();
+    //on keyup, start the countdown
+    //
+    var typingTimer;                //timer identifier
+    var doneTypingInterval = 300;  //time in ms, 5 second for example
+    var $input = $('input[name="idcard_no"]');
+    $input.keyup( function () {
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    });
 
-    $('input[name="idcard_no"]').keyup(
-            function(){
-                if ($('input[name="idcard_no"]').val().length == 10 ) {
-                    $('#collect_form').submit();
-                    $('input[name="idcard_no"]').val('');
-                }
-            });
+    //on keydown, clear the countdown
+    $input.keydown( function () {
+        clearTimeout(typingTimer);
+    });
 
-    $('#stu_reg').click(
-        function(){
-          $('.register.modal').modal('show');
-        }
-        );
+    //user is "finished typing," do something
+    function doneTyping () {
+        //do something
+        $('#collect_form').submit();
+        $('input[name="idcard_no"]').val('');
+    }
 });
