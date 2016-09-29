a8daba9566cc83342e73306ec53c96e276b0e18e
Daniel Tsai <idaniel.twc@gmail.com>
2016-09-29 18:31:23 +0800

    Make collect points faster

--- static/js/collect_points.js
+++ static/js/collect_points.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
     //on keyup, start the countdown
     //
     var typingTimer;                //timer identifier
-    var doneTypingInterval = 300;  //time in ms, 5 second for example
+    var doneTypingInterval = 100;  //time in ms, 5 second for example
     var $input = $('input[name="idcard_no"]');
     $input.keyup( function () {
         clearTimeout(typingTimer);
