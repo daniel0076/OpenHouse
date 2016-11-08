7290d75b82d42569266ba70e53f8120cfc5a3d44
Daniel Tsai <idaniel.twc@gmail.com>
2016-11-08 17:49:39 +0800

    Add SignupInline into mentor admin

--- careermentor/admin.py
+++ careermentor/admin.py
@@ -5,14 +5,18 @@ admin.AdminSite.site_header="OpenHouse 管理後台"
 admin.AdminSite.site_title="OpenHouse"
 admin.AdminSite.index_template="admin/admin_index.html"
 
+class SignupInline(admin.TabularInline):
+    model = models.Signup
+    extra = 0
 # Register your models here.
 @admin.register(models.Mentor)
 class CareerMentorAdmin(admin.ModelAdmin):
+    inlines = (SignupInline,)
     list_display=('title', 'company', 'date','start_time','end_time',
                   'mentor', 'place','limit', 'updated' )
 
 @admin.register(models.Signup)
-class CareerMentorAdmin(admin.ModelAdmin):
+class CareerSignupAdmin(admin.ModelAdmin):
     list_display=('mentor', 'name', 'student_id','dep','phone',
                   'email', 'time_available','question', 'updated' )
 
